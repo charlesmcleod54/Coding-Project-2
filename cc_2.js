d4e0bb6f6852eba213cd5bd48f19c8755f939d0c
Charles McLeod <charlesmcleod@usf.edu>
2025-01-26 11:10:58 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,13 @@ let products = ['Helmets', 'Footballs', 'Cleats', 'Gloves', 'Mouthpieces'];
 products.push('Pads');
 products.pop();
 console.log(products);
+// Task 2
+let scores = [78, 95, 87, 83, 90];
+scores[1] = 97;
+let total = 0;
+for (let score of scores) {
+    total += score;
+}
+let average = total / scores.length;
+console.log("Updates Scores: " + scores);
+console.log("Average Score: " + average.toFixed(2));
